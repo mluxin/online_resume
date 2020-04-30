5646d8af7fc59161a98ffdafd18b96904eebdb80
Rinaroar <marina.luxin@gmail.com>
2020-04-30 19:31:00 +0200

    remove App.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css';
+/* import './App.css'; */
 import {BrowserRouter as Router, Route} from "react-router-dom";
 
 import { ThemeProvider } from 'styled-components';
